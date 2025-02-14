d8c1e245634b428195f8a3b5c636546d470321e9
Leo Conforti <leoconforti@ltgk.net>
2025-02-14 16:14:49 +0000

    only include source files in coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     test: {
         include: ["./test/**/*.test.ts"],
         globals: true,
-        coverage: { provider: "v8" },
+        coverage: { provider: "v8", include: ["src/**/*.ts"] },
         reporters: ["default", "hanging-process", ["junit", { outputFile: "coverage/junit.xml" }]],
     },
     resolve: {
