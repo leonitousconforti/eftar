c5b90fff12f78790f5c4bc0fac81d12dfafb1bf5
Leo Conforti <leoconforti@ltgk.net>
2025-04-24 14:44:55 +0000

    upgrade effect build utils

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,3 @@
-import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -7,10 +6,5 @@ export default defineConfig({
         setupFiles: ["./test/vitest.setup.ts"],
         coverage: { provider: "v8", include: ["src/**/*.ts"], reporter: ["cobertura", "text"] },
         reporters: ["default", "hanging-process", ["junit", { outputFile: "./coverage/junit.xml" }]],
-    },
-    resolve: {
-        alias: {
-            eftar: path.resolve(__dirname, "src"),
-        },
     },
 });
