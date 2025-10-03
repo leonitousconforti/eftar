aa1929a8fafe452aeb08959dc80393399d6ba308
Leo Conforti <leo@leoconforti.us>
2025-10-03 01:40:00 -0500

    fix codegen

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
 /**
+ * @since 1.0.0
+ */
+
+/**
  * Shared GNU ustar tar header details.
  *
  * @since 1.0.0
  */
-export * as Header from "./Header.js"
+export * as Header from "./Header.ts"
 
 /**
  * GNU ustar tar implementation.
  *
  * @since 1.0.0
  */
-export * as Tar from "./Tar.js"
+export * as Tar from "./Tar.ts"
 
 /**
  * GNU ustar untar implementation.
  *
  * @since 1.0.0
  */
-export * as Untar from "./Untar.js"
+export * as Untar from "./Untar.ts"
